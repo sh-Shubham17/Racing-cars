c1d6e367a5dd32da99dad6678731b81a9e8e286b
shekhar17-sketch <61670651+shekhar17-sketch@users.noreply.github.com>
2021-03-14 17:17:07 +0530

    Update getImages.js

--- getImages.js
+++ getImages.js
@@ -2,12 +2,12 @@ let racingCars = [];
 window.addEventListener("load", () => {
   racingCars = String(sessionStorage.getItem("stringOfCars")).split(",");
   console.log(racingCars);
-  let obj = document.getElementById("insert");
+  let obj;
   let s = "";
   for (let i = 0; i < 3; i++) {
-    s += `<img class="car" id="car${i + 1}" src="${racingCars[i]}">`;
-    console.log(racingCars[i]);
+    obj = document.getElementById(`CAR${i + 1}`);
+    s = `<img class="car" id="car${i + 1}" src="${racingCars[i]}">`;
+    obj.innerHTML = s;
+    console.log(s);
   }
-  obj.innerHTML = s;
-  console.log(s);
 });
